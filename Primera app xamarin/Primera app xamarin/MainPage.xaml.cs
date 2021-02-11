d3f9c3f337716d12d038baf17b466c80bd1ad554
Mendo-hub <56095719+Mendo-hub@users.noreply.github.com>
2021-02-10 22:26:46 -0400

    commit update

---
+++ Primera app xamarin/Primera app xamarin/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Primera_app_xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
